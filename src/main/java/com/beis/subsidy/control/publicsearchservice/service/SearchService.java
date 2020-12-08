25c043eafe44b6fd0f10d33faae7327e358cafe9
Dhruvesh Patel <dhruvesh.patel@cognizant.com>
2020-12-08 10:33:38 +0000

    Removed unwanted if for asc

--- src/main/java/com/beis/subsidy/control/publicsearchservice/service/SearchService.java
+++ src/main/java/com/beis/subsidy/control/publicsearchservice/service/SearchService.java
@@ -161,9 +161,7 @@ public class SearchService {
 	 */
 	private Sort.Direction getSortDirection(String direction) {
 	    
-		if (direction.equals("asc")) {
-	      return Sort.Direction.ASC;
-	    } else if (direction.equals("desc")) {
+		if (direction.equals("desc")) {
 	      return Sort.Direction.DESC;
 	    }
 
