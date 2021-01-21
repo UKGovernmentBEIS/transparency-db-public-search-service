20466a5aa5add9414ff9bb580eb95283378a4602
KOTLAPRA2711 <prashanth.kotla@cognizant.com>
2021-01-21 20:06:09 +0530

    feat:adding reason text field trim in the response

--- src/main/java/com/beis/subsidy/control/publicsearchservice/controller/response/AwardResponse.java
+++ src/main/java/com/beis/subsidy/control/publicsearchservice/controller/response/AwardResponse.java
@@ -90,7 +90,7 @@ public class AwardResponse {
             this.createdTimestamp = award.getCreatedTimestamp();
             this.lastModifiedTimestamp = award.getLastModifiedTimestamp();
             this.publishedAwardDate = award.getPublishedAwardDate();
-            this.rejectReason = award.getReason()!= null ?  award.getReason(): null;
+            this.rejectReason = award.getReason()!= null ?  award.getReason().trim(): null;
         }
         this.beneficiary = new BeneficiaryResponse(award.getBeneficiary(),flag);
         this.subsidyMeasure = new SubsidyMeasureResponse(award.getSubsidyMeasure(),flag);
