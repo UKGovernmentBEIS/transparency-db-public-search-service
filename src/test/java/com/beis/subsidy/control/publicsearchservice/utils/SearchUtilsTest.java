920268102322b7e3f07048f1df95c9e0272d1ef8
KOTLAPRA2711 <prashanth.kotla@cognizant.com>
2021-01-21 23:05:18 +0530

    feat:adding full amount range for ending with > scenario

--- src/test/java/com/beis/subsidy/control/publicsearchservice/utils/SearchUtilsTest.java
+++ src/test/java/com/beis/subsidy/control/publicsearchservice/utils/SearchUtilsTest.java
@@ -78,7 +78,7 @@ public class SearchUtilsTest {
         assertThat(formatAmountRange).isNotNull();
         assertThat(formatAmountRange).isEqualTo("0");
 
-        String dataIp5 ="1000000 or more";
+        String dataIp5 ="1000000 >";
         formatAmountRange = SearchUtils.formatedFullAmountRange(dataIp5);
         assertThat(formatAmountRange).isNotNull();
         assertThat(formatAmountRange.contains("or more")).isTrue();
