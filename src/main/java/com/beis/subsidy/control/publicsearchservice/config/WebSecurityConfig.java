ae30b5d8b94c17b32ff6f50e6666be303f6ee121
chhagan19 <chhagan.sahu@cognizant.com>
2021-01-26 13:21:26 +0000

    code changes for security headers

--- src/main/java/com/beis/subsidy/control/publicsearchservice/config/WebSecurityConfig.java
+++ src/main/java/com/beis/subsidy/control/publicsearchservice/config/WebSecurityConfig.java
@@ -1,21 +1,20 @@
 package com.beis.subsidy.control.publicsearchservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends
+        WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.headers()
-             .frameOptions().sameOrigin()
-             .httpStrictTransportSecurity().disable()
-             .addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy","default-src 'self'"))
-             .addHeaderWriter(new StaticHeadersWriter("X-WebKit-CSP","default-src 'self'"))
-             .xssProtection();
     }
 }
