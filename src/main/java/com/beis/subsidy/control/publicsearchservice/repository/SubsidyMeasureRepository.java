d54409e519fe82fe5f4188538c235d28624e41e4
James Holder <james.holder@cgi.com>
2021-12-20 14:47:13 +0000

    Add SubsidyMeasureRepository

---
+++ src/main/java/com/beis/subsidy/control/publicsearchservice/repository/SubsidyMeasureRepository.java
@@ -0,0 +1,13 @@
+package com.beis.subsidy.control.publicsearchservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.beis.subsidy.control.publicsearchservice.model.SubsidyMeasure;
+
+/**
+ * Interface for Award repository to get award details from database
+ *
+ */
+public interface SubsidyMeasureRepository extends JpaRepository<SubsidyMeasure, Long>, JpaSpecificationExecutor<SubsidyMeasure> {
+}
