ea4924c9e3db3cb65262c0253931b1ba22aaa13c
James Holder Barker <james.holderbarker@cgi.com>
2026-07-17 08:18:23 +0100

    feat: Update keyword filtering

--- src/main/java/com/beis/subsidy/control/publicsearchservice/utils/MfaAwardSpecifications.java
+++ src/main/java/com/beis/subsidy/control/publicsearchservice/utils/MfaAwardSpecifications.java
@@ -1,9 +1,13 @@
 package com.beis.subsidy.control.publicsearchservice.utils;
 
 import com.beis.subsidy.control.publicsearchservice.controller.request.Filter;
+import com.beis.subsidy.control.publicsearchservice.model.Award;
 import com.beis.subsidy.control.publicsearchservice.model.MFAAward;
+import com.beis.subsidy.control.publicsearchservice.model.SubsidyMeasure;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,15 +18,29 @@ public class MfaAwardSpecifications {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
+            // Force a left join for mfa groupings, without this, awards without groupings will be omitted from the results.
+            Join<Award, SubsidyMeasure> mfaGroupingJoin =
+                    root.join("mfaGrouping", JoinType.LEFT);
+
             predicates.add(criteriaBuilder.equal(root.get("status"), "Published"));
 
             if (filter != null) {
                 if (hasText(filter.getKeyword())) {
+                    int keywordNumber;
+                    try {
+                        keywordNumber = Integer.parseInt(filter.getKeyword());
+                    }
+                    catch (NumberFormatException e) {
+                        keywordNumber = 0;
+                    }
                     String keyword = "%" + filter.getKeyword().toLowerCase().trim() + "%";
 
                     predicates.add(criteriaBuilder.or(
-                            //TODO: Identify which filters DBT want to search on for MFA awards
-                            criteriaBuilder.like(criteriaBuilder.lower(root.get("recipientName")), keyword)
+                            criteriaBuilder.like(criteriaBuilder.lower(root.get("recipientName")), keyword),
+                            criteriaBuilder.equal((root.get("mfaAwardNumber")), keywordNumber),
+                            criteriaBuilder.like(criteriaBuilder.lower(mfaGroupingJoin.get("mfaGroupingNumber")), keyword),
+                            criteriaBuilder.like(criteriaBuilder.lower(mfaGroupingJoin.get("mfaGroupingName")), keyword),
+                            criteriaBuilder.like(criteriaBuilder.lower(root.get("grantingAuthority").get("grantingAuthorityName")), keyword)
                     ));
                 }
 
